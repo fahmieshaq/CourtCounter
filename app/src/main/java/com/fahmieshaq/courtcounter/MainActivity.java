3f0035a0b3f485953a81812e983507916ac9fea1
Fahmi Eshaq <fjamcom@gmail.com>
2017-06-02 10:03:35 -0400

    Added nested layouts

--- app/src/main/java/com/fahmieshaq/courtcounter/MainActivity.java
+++ app/src/main/java/com/fahmieshaq/courtcounter/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     private final int TWO_POINTS = 2;
     private final int ONE_POINT = 1;
     private int teamAScore = 0;
+    private int teamBScore = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,11 +34,36 @@ public class MainActivity extends AppCompatActivity {
         displayForTeamA(teamAScore);
     }
 
+    // TEAM B
+
+    public void addThreePointsForTeamB(View view) {
+        teamBScore = teamBScore + THREE_POINTS;
+        displayForTeamB(teamBScore);
+    }
+
+    public void addTwoPointsForTeamB(View view) {
+        teamBScore = teamBScore + TWO_POINTS;
+        displayForTeamB(teamBScore);
+    }
+
+    public void addOnePointForTeamB(View view) {
+        teamBScore = teamBScore + ONE_POINT;
+        displayForTeamB(teamBScore);
+    }
+
     /**
-     * Display scores of basketball teams
+     * Display team A scores of basketball teams
      */
     private void displayForTeamA(int score) {
         TextView scoreValue = (TextView) findViewById(R.id.team_a_score);
+        scoreValue.setText(String.valueOf(score));
+    }
+
+    /**
+     * Display team B scores of basketball teams
+     */
+    private void displayForTeamB(int score) {
+        TextView scoreValue = (TextView) findViewById(R.id.team_b_score);
         scoreValue.setText(String.valueOf(score));
     }
 }
