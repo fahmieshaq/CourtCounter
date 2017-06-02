2cc30f2d7433b38bfec7d30b69544dfd718a2d2d
Fahmi Eshaq <fjamcom@gmail.com>
2017-06-02 09:28:53 -0400

    Added three onClick events

--- app/src/main/java/com/fahmieshaq/courtcounter/MainActivity.java
+++ app/src/main/java/com/fahmieshaq/courtcounter/MainActivity.java
@@ -2,12 +2,39 @@ package com.fahmieshaq.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int teamAScore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void addThreePointsForTeamA(View view) {
+        teamAScore = 3;
+        displayForTeamA(teamAScore);
+    }
+
+    public void addTwoPointsForTeamA(View view) {
+        teamAScore = 2;
+        displayForTeamA(teamAScore);
+    }
+
+    public void addOnePointForTeamA(View view) {
+        teamAScore = 1;
+        displayForTeamA(teamAScore);
+    }
+
+    /**
+     * Display scores of basketball teams
+     */
+    private void displayForTeamA(int score) {
+        TextView scoreValue = (TextView) findViewById(R.id.team_a_score);
+        scoreValue.setText(String.valueOf(score));
     }
 }
