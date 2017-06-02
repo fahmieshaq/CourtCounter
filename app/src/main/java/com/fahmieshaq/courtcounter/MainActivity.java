8e4b323e7e30a379094293499147ebe2cfe1119b
Fahmi Eshaq <fjamcom@gmail.com>
2017-06-02 17:34:51 -0400

    Added old score keeper

--- app/src/main/java/com/fahmieshaq/courtcounter/MainActivity.java
+++ app/src/main/java/com/fahmieshaq/courtcounter/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     private final int THREE_POINTS = 3;
     private final int TWO_POINTS = 2;
     private final int ONE_POINT = 1;
+
     private int teamAScore = 0;
     private int teamBScore = 0;
 
@@ -19,43 +20,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    // Team A
+    // Team A score calculator
     public void addThreePointsForTeamA(View view) {
         teamAScore = teamAScore + THREE_POINTS;
         displayForTeamA(teamAScore);
     }
 
+    // Team A score calculator
     public void addTwoPointsForTeamA(View view) {
         teamAScore = teamAScore + TWO_POINTS;
         displayForTeamA(teamAScore);
     }
 
+    // Team A score calculator
     public void addOnePointForTeamA(View view) {
         teamAScore = teamAScore + ONE_POINT;
         displayForTeamA(teamAScore);
     }
 
-    // TEAM B
+    // Team B score calculator
     public void addThreePointsForTeamB(View view) {
         teamBScore = teamBScore + THREE_POINTS;
         displayForTeamB(teamBScore);
     }
 
+    // Team B score calculator
     public void addTwoPointsForTeamB(View view) {
         teamBScore = teamBScore + TWO_POINTS;
         displayForTeamB(teamBScore);
     }
 
+    // Team B score calculator
     public void addOnePointForTeamB(View view) {
         teamBScore = teamBScore + ONE_POINT;
-        displayForTeamB(teamBScore);
-    }
-
-    // Reset scores
-    public void reset(View view) {
-        teamAScore = 0;
-        teamBScore = 0;
-        displayForTeamA(teamAScore);
         displayForTeamB(teamBScore);
     }
 
@@ -73,5 +70,32 @@ public class MainActivity extends AppCompatActivity {
     private void displayForTeamB(int score) {
         TextView scoreValue = (TextView) findViewById(R.id.team_b_score);
         scoreValue.setText(String.valueOf(score));
+    }
+
+    /**
+     * Display team A old score
+     */
+    private void displayOldScoreForTeamA(int score) {
+        TextView oldScoreValue = (TextView) findViewById(R.id.team_a_history_score);
+        oldScoreValue.setText(String.valueOf(score));
+    }
+
+    /**
+     * Display team B old score
+     */
+    private void displayOldScoreForTeamB(int score) {
+        TextView oldScoreValue = (TextView) findViewById(R.id.team_b_history_score);
+        oldScoreValue.setText(String.valueOf(score));
+    }
+
+    // Reset scores
+    public void reset(View view) {
+        // Save old scores and then reset scores
+        displayOldScoreForTeamA(teamAScore);
+        displayOldScoreForTeamB(teamBScore);
+        teamAScore = 0;
+        teamBScore = 0;
+        displayForTeamA(teamAScore);
+        displayForTeamB(teamBScore);
     }
 }
